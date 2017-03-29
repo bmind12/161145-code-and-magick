4728bedc2071db6910be52c0326cf477dca7d975
Sergey R <ryabovs@yahoo.com>
2017-03-29 13:13:32 +0200

    Refactoring. drawBarChart function rewritten to be less complex

--- js/stat.js
+++ js/stat.js
@@ -1,65 +1,98 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  var top = 20;
-  var left = 100;
+  var topCloudPosition = 20;
+  var leftCloudPosition = 100;
 
-  drawCloudShadow(ctx, left, top);
-  drawCloud(ctx, left, top);
+  drawCloudShadow(ctx, leftCloudPosition, topCloudPosition);
+  drawCloud(ctx, leftCloudPosition, topCloudPosition);
   printCongrats(ctx);
   drawBarChart(ctx, names, times);
 };
 
-function drawCloudShadow(ctx, left, top) {
+function drawCloudShadow(ctx, leftCloudPosition, topCloudPosition) {
   var shadowShift = 10;
-  drawCloudShape(ctx, left + shadowShift, top + shadowShift, 'rgba(0, 0, 0, 0.7)');
+  var shadowColor = 'rgba(0, 0, 0, 0.7)';
+
+  drawCloudShape(ctx, leftCloudPosition + shadowShift, topCloudPosition + shadowShift, shadowColor);
 }
 
-function drawCloud(ctx, left, top) {
-  drawCloudShape(ctx, left, top, 'rgb(255, 255, 255)');
+function drawCloud(ctx, leftCloudPosition, topCloudPosition) {
+  var cloudColor = 'rgb(255, 255, 255)';
+
+  drawCloudShape(ctx, leftCloudPosition, topCloudPosition, cloudColor);
 }
 
-function drawCloudShape(ctx, left, top, color) {
+function drawCloudShape(ctx, leftCloudPosition, topCloudPosition, color) {
   var width = 420;
   var height = 270;
+
   ctx.fillStyle = color;
-  ctx.fillRect(left, top, width, height);
+  ctx.fillRect(leftCloudPosition, topCloudPosition, width, height);
 }
 
 function printCongrats(ctx) {
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000000';
-  ctx.fillText('Ура, вы победили!', 140, 40);
+  var congratsFont = '16px PT Mono';
+  var congratsColor = '#000000';
+  var congratsText = 'Ура, вы победили!';
+  var congratsTopPosition = 40;
+  var congratsLeftPosition = 140;
+
+  ctx.font = congratsFont;
+  ctx.fillStyle = congratsColor;
+  ctx.fillText(congratsText, congratsLeftPosition, congratsTopPosition);
 }
 
 function drawBarChart(ctx, names, times) {
-  var left = 140;
-  var barTop = 245;
-  var timeTop = 240;
-  var nameTop = 265;
-  var barHeightMax = 150;
+  var x = 140;
+  var y = 70;
+  var barWidth = 40;
   var barHeight = 150;
-  var barWirdth = 40;
-  var indent = 50;
+  var barHeightMax = 150;
   var maxTime = findMaxTime(times);
-  var step = barHeightMax / maxTime;
+  var barHeightStep = barHeightMax / maxTime;
+  var yIndent = 10;
+  var xIndent = 50;
 
-  // Add chart's title
-  ctx.fillText('Список результатов:', left, 60);
+  addChartTitle(ctx, x, y);
 
-  // Add chart's bars
   for (var i = 0; i < times.length; i++) {
 
-    // Draw a bar
-    pickBarColor(ctx, names[i]);
-    barHeight = step * times[i];
-    ctx.fillRect(left + (barWirdth + indent) * i, barTop - barHeight, barWirdth, barHeight);
+    x = 140;
+    y = 90;
 
-    // Add bar labels
-    ctx.fillStyle = '#000000';
-    ctx.fillText(times[i].toFixed(0), left + (barWirdth + indent) * i, timeTop - barHeight);
-    ctx.fillText(names[i], left + (barWirdth + indent) * i, nameTop);
+    x = addPadding(x, (barWidth + xIndent) * i);
+    barHeight = barHeightStep * times[i];
+
+    y = addPadding(y, yIndent + barHeightMax - barHeight);
+    addBarLabel(ctx, times[i].toFixed(), x, y);
+
+    y = addPadding(y, yIndent);
+    pickBarColor(ctx, names[i]);
+    drawBar(ctx, x, y, barWidth, barHeight);
+
+    y = addPadding(y, barHeight + yIndent * 2);
+    addBarLabel(ctx, names[i], x, y);
   }
+}
+
+function addChartTitle(ctx, x, y) {
+  var title = 'Список результатов:';
+  ctx.fillText(title, x, y);
+}
+
+function drawBar(ctx, x, y, width, height) {
+  ctx.fillRect(x, y, width, height);
+}
+
+function addBarLabel(ctx, text, x, y) {
+  ctx.fillStyle = '#000000';
+  ctx.fillText(text, x, y);
+}
+
+function addPadding(x, padding) {
+  x += padding;
+  return x;
 }
 
 function findMaxTime(times) {
