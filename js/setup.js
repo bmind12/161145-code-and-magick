26ec29a1776ce41e6185dd78498c7af44adb1cb4
Sergey R <ryabovs@yahoo.com>
2017-04-06 21:07:53 +0200

    Similar characters generated and displayed appended to setup window

--- js/setup.js
+++ js/setup.js
@@ -1,44 +1,71 @@
 'use strict';
 
-var CHARACTERS_LIST_LENGTH = 4;
-var CHARACTERS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var CHARACTERS_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var CHARACTERS_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var CHARACTERS_EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var CHARS_LIST_LENGTH = 4;
+var NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+var SURNAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+var COAT_COLORS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+var EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_TEMPLATE = 'similar-wizard-template';
+
 
 var setupElement = document.querySelector('.setup');
 setupElement.classList.remove('hidden');
-
 var charactersList = generateSimilarCharacters();
-
-var charactersHTML = generateCharactersHTML(charactersList);
+var charactersHTML = generateCharactersHTML(charactersList, WIZARD_TEMPLATE);
 document.querySelector('.setup-similar-list').appendChild(charactersHTML);
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 function generateSimilarCharacters() {
-  var charactersList = [];
+  var similarCharacters = [];
 
-  for (var i = 0; i < CHARACTERS_LIST_LENGTH; i++) {
-    var characterName = CHARACTERS_NAMES[getRandomIntInclusive(0, CHARACTERS_NAMES.length - 1)];
-    var characterSurname = CHARACTERS_SURNAMES[getRandomIntInclusive(0, CHARACTERS_SURNAMES.length - 1)];
-    var characterCoatColor = CHARACTERS_COAT_COLORS[getRandomIntInclusive(0, CHARACTERS_COAT_COLORS.length - 1)];
-    var characterEyeColor = CHARACTERS_EYE_COLORS[getRandomIntInclusive(0, CHARACTERS_EYE_COLORS.length - 1)];
+  for (var i = 0; i < CHARS_LIST_LENGTH; i++) {
 
-    var newCharacter = new Charcter(characterName, characterSurname, characterCoatColor, characterEyeColor);
-    charactersList.push(newCharacter);
+    var newCharacter = createCharcter({
+      charName: NAMES[getRandomInt(0, NAMES.length - 1)],
+      charSurname: SURNAMES[getRandomInt(0, SURNAMES.length - 1)],
+      charCoatColor: COAT_COLORS[getRandomInt(0, COAT_COLORS.length - 1)],
+      charEyeColor: EYE_COLORS[getRandomInt(0, EYE_COLORS.length - 1)],
+    });
+
+    similarCharacters.push(newCharacter);
   }
 
-  return charactersList;
+  return similarCharacters;
 }
 
-function generateCharactersHTML(list) {
+function generateCharactersHTML(list, templateClass) {
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < list.length; i++) {
-    var template = document.querySelector('#similar-wizard-template').content.cloneNode(true);
-    template.querySelector('.setup-similar-label').textContent = list[i].name;
-    template.querySelector('.wizard-coat').style.fill = list[i].coatColor;
-    template.querySelector('.wizard-eyes').style.fill = list[i].eyeColor;
+    var template =
+      document.querySelector('#' + templateClass).content.cloneNode(true);
+
+    fillCharacterTemplate(template, list[i]);
 
     fragment.appendChild(template);
   }
@@ -46,14 +73,24 @@ function generateCharactersHTML(list) {
   return fragment;
 }
 
-function Charcter(name, surname, coatColor, eyeColor) {
-  this.name = name;
-  this.surname = surname;
-  this.coatColor = coatColor;
-  this.eyeColor = eyeColor;
+function fillCharacterTemplate(template, charData) {
+  template.querySelector('.setup-similar-label').textContent = charData.name;
+  template.querySelector('.wizard-coat').style.fill = charData.coatColor;
+  template.querySelector('.wizard-eyes').style.fill = charData.eyeColor;
 }
 
-function getRandomIntInclusive(min, max) {
+function createCharcter(charData) {
+  var character = {
+    name: charData.charName,
+    surname: charData.charSurname,
+    coatColor: charData.charCoatColor,
+    eyeColor: charData.charEyeColor,
+  };
+
+  return character;
+}
+
+function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
 
